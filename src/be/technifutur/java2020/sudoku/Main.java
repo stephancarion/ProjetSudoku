b3532af8a924ecdfc265911873e9a0cb8ceffc95
Stephan <stephan.carion@gmail.com>
2020-10-14 09:24:13 +0200

    test2

--- src/be/technifutur/java2020/sudoku/Main.java
+++ src/be/technifutur/java2020/sudoku/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sudoku");
 
-// test
+
         Sudoku4x4 s1 = new Sudoku4x4();
         Sudoku9x9 s2 = new Sudoku9x9();
         SudokuEtoile s3 = new SudokuEtoile();
