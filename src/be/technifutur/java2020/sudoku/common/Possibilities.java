ed07ef994b116860aff8b7fd6bc4e14d5eb15afc
Stephan <stephan.carion@gmail.com>
2020-10-22 14:28:13 +0200

    class Possibilities : commentaire fonctionnement classe

--- src/be/technifutur/java2020/sudoku/common/Possibilities.java
+++ src/be/technifutur/java2020/sudoku/common/Possibilities.java
@@ -1,4 +1,12 @@
 package be.technifutur.java2020.sudoku.common;
 
-public class Possibilities {
-}
+/*
+ Cette classe gère les posibilités pour le sudoku de 1 à 9 sous la forme d'un code binaire
+    - le code 0 signifie que la possibilite n'existe pas
+    - le code 1 signifie que la possibilite existe
+ L'écriture binaire écrite de droite à gauche correspond respectivement aux possibilités des valeurs de 1 à 9
+
+ -- Exemple: 0b001101001 signifie que les valeurs 1,4,6 et 7 sont possibles
+ */
+
+public class Possibilities {}
