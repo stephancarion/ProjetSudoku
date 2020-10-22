673802cbf189225fed187add9e3d41f05ba25b93
Stephan <stephan.carion@gmail.com>
2020-10-22 14:35:36 +0200

    class Possibilities : variable d'instance + implémentation du constructeur

--- src/be/technifutur/java2020/sudoku/common/Possibilities.java
+++ src/be/technifutur/java2020/sudoku/common/Possibilities.java
@@ -2,11 +2,18 @@ package be.technifutur.java2020.sudoku.common;
 
 /*
  Cette classe gère les posibilités pour le sudoku de 1 à 9 sous la forme d'un code binaire
-    - le code 0 signifie que la possibilite n'existe pas
-    - le code 1 signifie que la possibilite existe
+    - le code 0 signifie que la possibilité n'existe pas
+    - le code 1 signifie que la possibilité existe
  L'écriture binaire écrite de droite à gauche correspond respectivement aux possibilités des valeurs de 1 à 9
 
  -- Exemple: 0b001101001 signifie que les valeurs 1,4,6 et 7 sont possibles
  */
 
-public class Possibilities {}
+public class Possibilities {
+    private int data = 0;
+
+    // Par défaut, toutes les valeurs de 1 à 9 sont possibles à l'instanciation
+    public Possibilities(){
+        data=0b111111111;
+    }
+}
